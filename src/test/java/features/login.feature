777d641e5f9f52451252f421d64254e6fac177dc
huongnguyen <huongnguyen@PC-HUONGNGUYEN.vodc.lan>
2018-06-29 14:17:45 +0700

    commit

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -4,8 +4,7 @@ Feature: CREATE ACCOUNT
   I want to create an Account
   So that I can verify register function work well
 
-
-  Scenario: Create new Account
+  Scenario Outline: Create new Account
     Given I open browser
     And I get login url
     When I click to here link
@@ -17,5 +16,16 @@ Feature: CREATE ACCOUNT
     And I input to username textbox
     And I input to password textbox
     And I input to submit button at login page
-    Then I verify homepage welcome message displayed
+    When I click to New Customer Page
+    And I input all information to this page
+      | CustomerName   | Gender   | DateOfBirth   | Adress   | City   | State   | Pin   | Phone   | Email   | Password   |
+      | <CustomerName> | <Gender> | <DateOfBirth> | <Adress> | <City> | <State> | <Pin> | <Phone> | <Email> | <Password> |
+    And I click to submit button
+    And I verify customer created success with information
+      | CustomerName   | Gender   | DateOfBirth   | Adress   | City   | State   | Pin   | Phone   | Email   | Password   |
+      | <CustomerName> | <Gender> | <DateOfBirth> | <Adress> | <City> | <State> | <Pin> | <Phone> | <Email> | <Password> |
     And I close browser
+
+    Examples: 
+      | CustomerName    | Gender | DateOfBirth | Adress      | City    | State    | Pin    | Phone     | Email | Password |
+      | Automation Test | male      | 01/02/1995  | 123 Address | Da Nang | Hai Chau | 123456 | 123456789 | auto  |   111111 |
--- src/test/java/stepDefinitions/LoginPageSteps.java
+++ src/test/java/stepDefinitions/LoginPageSteps.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -11,7 +13,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import cucumber.api.java.en.And;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -20,6 +22,7 @@ public class LoginPageSteps {
 	WebDriver driver;
 	WebDriverWait wait;
 	String username, password, loginUrl, welcomeMessage;
+	int emailUnique = randomEmail();
 	
 	@Given("^I open browser$")
 	public void iOpenBrowser()  {
@@ -27,22 +30,17 @@ public class LoginPageSteps {
 		driver.get("http://demo.guru99.com/v4/");
 		wait = new WebDriverWait(driver, 30);
 		driver.manage().window().maximize();		
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);	    
-	    ;
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);	
 	}
 
 	@Given("^I get login url$")
 	public void iGetLoginUrl()  {
 		loginUrl = driver.getCurrentUrl();
-	    
-	    ;
 	}
 
 	@When("^I click to here link$")
 	public void iClickToHereLink()  {
 		driver.findElement(By.xpath("//a[text()='here']")).click();
-	    
-	    ;
 	}
 
 	@When("^I input to email textbox$")
@@ -50,16 +48,12 @@ public class LoginPageSteps {
 		WebElement emailTextbox = driver.findElement(By.xpath("//input[@name='emailid']"));
 		waitForElementVisible(emailTextbox);
 		emailTextbox.sendKeys("automation"+randomEmail()+"@gmail.com");
-	    
-	    ;
 	}
 
 	@When("^I click to submit button at register page$")
 	public void iClickToSubmitButtonAtRegisterPage()  {
 		WebElement submitButon = driver.findElement(By.xpath("//input[@name='btnLogin']"));
 		submitButon.click();
-	    
-	    ;
 	}
 
 	@When("^I get to username information$")
@@ -67,22 +61,16 @@ public class LoginPageSteps {
 		WebElement usernameTextbox = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td"));
 		waitForElementVisible(usernameTextbox);
 		username = usernameTextbox.getText();
-		
-	    
-	    ;
 	}
 
 	@When("^I get to password information$")
 	public void iGetToPasswordInformation()  {
 		password = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();	
-	    
-	    ;
 	}
 
 	@When("^I open to login page$")
 	public void iOpenToLoginPage()  {
 	    driver.get(loginUrl);
-	    ;
 	}
 
 	@When("^I input to username textbox$")
@@ -90,35 +78,95 @@ public class LoginPageSteps {
 	    WebElement usernameTextbox = driver.findElement(By.xpath("//input[@name='uid']"));
 	    waitForElementVisible(usernameTextbox);
 	    usernameTextbox.sendKeys(username);
-	    ;
 	}
 
 	@When("^I input to password textbox$")
 	public void iInputToPasswordTextbox()  {
 		WebElement passwordTextbox = driver.findElement(By.xpath("//input[@name='password']"));
 		passwordTextbox.sendKeys(password);
-	    
-	    ;
 	}
 
 	@When("^I input to submit button at login page$")
 	public void iInputToSubmitButtonAtLoginPage()  {
 	    WebElement submitButton = driver.findElement(By.xpath("//input[@name='btnLogin']"));
 	    submitButton.click();
-	    ;
 	}
 
 	@Then("^I verify homepage welcome message displayed$")
 	public void iVerifyHomepageWelcomeMessageDisplayed()  {
 	    WebElement homePageWelcomeMessage = driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]"));
 	    Assert.assertTrue(homePageWelcomeMessage.isDisplayed());
-	    ;
+	}
+	
+	@When("^I click to New Customer Page$")
+	public void iClickToNewCustomerPage() {
+		WebElement newCustomer = driver.findElement(By.xpath("//a[text()='New Customer']"));
+		newCustomer.click();
+	    
+	}
+
+	@When("^I input all information to this page$")
+	public void iInputAllInformationToThisPage(DataTable table) {
+	    WebElement nameTextbox = driver.findElement(By.xpath("//input[@name='name']"));
+	    WebElement genderRadiobutton = driver.findElement(By.xpath("//input[@value='m']"));
+	    WebElement dateOfBirthTextbox = driver.findElement(By.xpath("//*[@id='dob']"));
+	    WebElement addressTextbox = driver.findElement(By.xpath("//textarea[@name='addr']"));
+	    WebElement cityTextbox = driver.findElement(By.xpath("//input[@name='city']"));
+	    WebElement stateTextbox = driver.findElement(By.xpath("//input[@name ='state']"));
+	    WebElement pinTextbox = driver.findElement(By.xpath("//input[@name='pinno']"));
+	    WebElement phoneTextbox = driver.findElement(By.xpath("//input[@name='telephoneno']"));
+	    WebElement emailIdTextbox = driver.findElement(By.xpath("//input[@name='emailid']"));
+	    WebElement passwordTextbox = driver.findElement(By.xpath("//input[@name='password']"));
+	    
+	    List<Map<String, String>> customer = table.asMaps(String.class, String.class);
+	    
+	    nameTextbox.sendKeys(customer.get(0).get("CustomerName"));
+	    genderRadiobutton.click();
+	    dateOfBirthTextbox.sendKeys(customer.get(0).get("DateOfBirth"));
+	    addressTextbox.sendKeys(customer.get(0).get("Adress"));
+	    cityTextbox.sendKeys(customer.get(0).get("City"));
+	    stateTextbox.sendKeys(customer.get(0).get("State"));
+	    pinTextbox.sendKeys(customer.get(0).get("Pin"));
+	    phoneTextbox.sendKeys(customer.get(0).get("Phone"));
+	    emailIdTextbox.sendKeys(customer.get(0).get("Email")+ emailUnique + "@gmail.com");
+	    passwordTextbox.sendKeys(customer.get(0).get("Password"));
+	    
+	}
+
+	@When("^I click to submit button$")
+	public void iClickToSubmitButton() {
+	   WebElement submitButton = driver.findElement(By.xpath("//input[@name='sub']"));
+	   submitButton.click();
+	    
+	}
+	
+	@When("^I verify customer created success with information$")
+	public void iVerifyCustomerCreatedSuccessWithInformation(DataTable table) {
+	    WebElement customerNameText = driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td"));
+	    WebElement genderText = driver.findElement(By.xpath("//td[text()='Gender']/following-sibling::td"));
+	    WebElement adressText = driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td"));
+	    WebElement cityText = driver.findElement(By.xpath("//td[text()='City']/following-sibling::td"));
+	    WebElement stateText = driver.findElement(By.xpath("//td[text()='State']/following-sibling::td"));
+	    WebElement pinText = driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td"));
+	    WebElement mobileText = driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td"));
+	    WebElement emailText = driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td"));
+	    
+	    List<Map<String, String>> customer = table.asMaps(String.class, String.class);
+	    Assert.assertEquals(customerNameText.getText(), customer.get(0).get("CustomerName"));
+	    Assert.assertEquals(genderText.getText(), customer.get(0).get("Gender"));
+	    Assert.assertEquals(adressText.getText(), customer.get(0).get("Adress"));
+	    Assert.assertEquals(cityText.getText(), customer.get(0).get("City"));
+	    Assert.assertEquals(stateText.getText(), customer.get(0).get("State"));
+	    Assert.assertEquals(pinText.getText(), customer.get(0).get("Pin"));
+	    Assert.assertEquals(mobileText.getText(), customer.get(0).get("Phone"));
+	    Assert.assertEquals(emailText.getText(), customer.get(0).get("Email")+ emailUnique + "@gmail.com");
+	    		
 	}
 	
 	@Then("^I close browser$")
 	public void iCloseBrowser()  {
 	    driver.quit();
-	    ;
+	   
 	}
 	
 	public void waitForElementVisible(WebElement element) {
